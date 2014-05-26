659462ed189198f99fb31bd637105207ff6c0657
peeyush <peeyushagarwal1994@gmail.com>
2014-05-26 17:08:20 +0530

    Pull actual log data from Log Manager API (hosted on Heroku) and send it to CODAP via Data Interactive API for visualization

--- Analytics.js
+++ Analytics.js
@@ -1,16 +1,56 @@
+var attrs = [];
+var attrs_obj = [];
+var cases = [];
+var single_case = []
 var Analytics = {
 
-  controller: window.parent.DG.currGameController,
+  controller: window.parent.DG,
 
   analyze: function() {
-    console.log("I am inside analyze function.");
+    // console.log("I am inside analyze function.");
     var this_ = this;
-    this_.doAnalysis();
+    $.ajax({ 
+      type: 'GET', 
+      url: 'https://log-manager.herokuapp.com/api/logs', 
+      success: function(data){
+        this_.doAnalysis(data)
+      }
+    });
   },
 
-  doAnalysis: function() {
-    var this_ = this;
-    console.log("I am inside do Analytics Function");
+  doAnalysis: function(data) {
+
+    // console.log("I am inside do Analytics Function");
+    var this_ = this,
+        kParentCollectionName = 'Logs';
+
+    for (var key in data) {
+      if (data.hasOwnProperty(key)) {
+        for ( var k in data[key] ){
+          if($.inArray(k, attrs) <= -1){
+            attrs.push(k);
+            attrs_obj.push({name: k});
+          }
+        }
+      }
+    }
+
+    for (var key in data) {
+      if (data.hasOwnProperty(key)){
+        single_case = [];
+        for (var i = 0; i < attrs.length; i++) {
+          if(attrs[i] in data[key]){
+            if (typeof(data[key][attrs[i]]) === "object")
+              single_case.push( JSON.stringify( data[key][attrs[i]] ) );
+            else
+              single_case.push( data[key][attrs[i]] );
+          }else{
+            single_case.push("");
+          }
+        }
+        cases.push(single_case);
+      }
+    }
 
     this_.controller.doCommand( {
       action: 'initGame',
@@ -23,22 +63,18 @@ var Analytics = {
     this_.controller.doCommand( {
       action: 'createCollection',
       args: {
-        name: 'Logs',
-        attrs: [{ name: 'cases' }, { name: 'Values'}],
+        name: kParentCollectionName,
+        attrs: attrs_obj,
         log: false
       }
     });
+
     this_.controller.doCommand( {
       action: 'createCases',
       args: {
         collection: "Logs",
-        values: [
-                  [ "Case 1", 2],
-                  [ "Case 2", 4],
-                  [ "Case 3", 6]
-                ]
+        values: cases
       }
     });
   }
-
 };
